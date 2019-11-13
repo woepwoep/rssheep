1679cb43de997614d36fe8c3b83f122052322f1c
Ronald Wopereis <woepwoep@gmail.com>
2019-11-13 17:40:43 +0100

    scrapie status char(7) ipv numeric

--- ext_tables.sql
+++ ext_tables.sql
@@ -6,17 +6,10 @@ CREATE TABLE tx_rssheep_domain_model_sheepdata (
 	life_number varchar(255) DEFAULT '' NOT NULL,
 	gender int(11) DEFAULT '0' NOT NULL,
 	birth_date int(11) DEFAULT NULL,
-	scrapie_status int(11) DEFAULT '0' NOT NULL,
+	scrapie_status varchar(7) DEFAULT '',
 	life_number_mother varchar(255) DEFAULT '' NOT NULL,
 	life_number_father varchar(255) DEFAULT '' NOT NULL,
 	remark text,
 	registrant int(11) DEFAULT '0' NOT NULL,
 
-);
-
-#
-# Table structure for table 'tx_rssheep_domain_model_sheepdata'
-#
-CREATE TABLE tx_rssheep_domain_model_sheepdata (
-	categories int(11) unsigned DEFAULT '0' NOT NULL,
 );
